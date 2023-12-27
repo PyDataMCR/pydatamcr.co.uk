7b9b44fb7dc23cee82089c34fa1421336f4bf691
jasper <jaspajjr@users.noreply.github.com>
2023-12-27 13:17:45 +0000

    Get events data

--- main.py
+++ main.py
@@ -3,16 +3,28 @@ from pyairtable.formulas import match
 import os
 
 
-def load_open_jobs(api: Api, table_id: str):
-    # formula = match({"Status": "Active"})
+def load_airtable_data(api: Api, table_id: str, formula: match):
     table = api.table(os.environ["BASE_ID"], table_id)
-    return table.all()
+    return table.all(formula=formula)
+
+
+def generate_jobs_formula() -> match:
+    return match({"Status": "Active"})
+
+
+def generate_events_formula() -> match:
+    return match({"Status": "Published"})
 
 
 def main() -> int:
     airtable_api = Api(os.environ["AIRTABLE_AUTH"])
-    open_jobs = load_open_jobs(airtable_api, "tblPOlCzOHDiu2NYC")
-    return open_jobs
+    open_jobs = load_airtable_data(
+        airtable_api, "tblPOlCzOHDiu2NYC", generate_jobs_formula()
+    )
+    upcoming_events = load_airtable_data(
+        airtable_api, "tblzcEGAmSqFskbae", generate_events_formula()
+    )
+    return upcoming_events
 
 
 if __name__ == "__main__":
