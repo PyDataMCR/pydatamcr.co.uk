c943135799f18816697a3360889e91d6f42be816
jasper <jaspajjr@users.noreply.github.com>
2023-12-27 23:26:01 +0000

    Added requirements

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from pyairtable import Api
 from pyairtable.formulas import match
 import os
@@ -24,8 +25,31 @@ def main() -> int:
     upcoming_events = load_airtable_data(
         airtable_api, "tblzcEGAmSqFskbae", generate_events_formula()
     )
-    return (open_jobs, airtable_api)
+    return open_jobs
+
+
+BUFFER_PATH = os.environ["GITHUB_OUTPUT"]
+
+
+def write_to_output(context: Dict[str, str]) -> None:
+    """writes the keys (as variables) and values (as values) to the output buffer
+
+    Args:
+        context: variables and values
+
+    Examples:
+        In your project, use this function like:
+
+        >>> write_to_output({"name": "John", ...})
+
+        ``name`` will be the variable name and ``John`` is the value.
+    """
+
+    with open(BUFFER_PATH, "a") as _buffer:
+        for var, val in context.items():
+            _buffer.write(f"{var}={val}\r\n")
 
 
 if __name__ == "__main__":
-    print(main())
+    # open_jobs = main()
+    write_to_output({"Name": "John"})
