e7e0942d8567b904e8af74fb860e380a135f87ff
Shaun Hide <shaunhide@Shauns-MacBook-Pro.local>
2024-03-23 11:00:27 +0000

    Updated to a minimal theme

--- website/content/pages/about.md
+++
@@ -1,7 +0,0 @@
-Title: About Us
-Date: 2024-03-23
-Slug: about-us
-save_as: about-us.html
-Url: about-us
-
-Write Content Here?
--- website/pelicanconf.py
+++ website/pelicanconf.py
@@ -8,6 +8,19 @@ TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = 'en'
 
+# Set the theme
+THEME = "themes/minimal/"
+
+# Static pages of the website that will be generated
+TEMPLATE_PAGES = {
+    "pages/index.html": "index.html",
+    "pages/404.html": "404.html",
+    "pages/about-us.html": "about-us.html",
+}
+
+# Static paths of the website
+STATIC_PATHS = ["images"]
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -15,21 +28,35 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
+# Settings for the URLs of the blog and the articles
+ARTICLE_PATHS = ["events"]
+ARTICLE_URL = "events/{slug}/"
+ARTICLE_SAVE_AS = "events/{slug}/index.html"
+INDEX_SAVE_AS = "events.html"
 
-# Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
+# Disbale unneeded blog features
+ARCHIVES_SAVE_AS = ""
+AUTHOR_SAVE_AS = ""
+AUTHORS_SAVE_AS = ""
+CATEGORY_SAVE_AS = ""
+CATEGORIES_SAVE_AS = ""
+TAGS_SAVE_AS = ""
+
+# # Blogroll
+# LINKS = (
+#     ("Pelican", "https://getpelican.com/"),
+#     ("Python.org", "https://www.python.org/"),
+#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+#     ("You can modify those links in your config file", "#"),
+# )
+
+# # Social widget
+# SOCIAL = (
+#     ("You can add links in your config file", "#"),
+#     ("Another social link", "#"),
+# )
 
 DEFAULT_PAGINATION = 50
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
